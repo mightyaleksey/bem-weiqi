a565e5d115077b724e16e7880ccaea85d34f55db
Alexey Litvinov <sullenor@yandex-team.ru>
2014-04-06 21:07:43 +0400

    Добавил небольшой хелпер для технологий

--- .enb/make.js
+++ .enb/make.js
@@ -1,63 +1,32 @@
 module.exports = function (config) {
     config.node('desktop.bundle', function (nodeConfig) {
         nodeConfig.addTechs([
-            /**
-             * Сборка bemtree по уровням
-             */
-            [
-                require('enb/techs/levels'),
-                {
-                    levels: desktopLevels()
-                }
-            ],
-            [
-                require('./techs/bemdecl-from-levels'),
-                {
-                    levels: bemtreeLevels(),
-                    target: '?.bemtree.bemdecl.js'
-                }
-            ],
-            [
-                require('enb/techs/deps'),
-                {
-                    bemdeclTarget: '?.bemtree.bemdecl.js',
-                    depsTarget: '?.bemtree.deps.js'
-                }
-            ],
-            [
-                require('enb/techs/files'),
-                {
-                    depsTarget: '?.bemtree.deps.js',
-                    filesTarget: '?.bemtree.files',
-                    dirsTarget: '?.bemtree.dirs'
-                }
-            ],
-            [
-                require('enb-bemxjst/techs/bemtree'),
-                {
-                    filesTarget: '?.bemtree.files'
-                }
-            ],
+            /* Сборка bemtree по уровням */
+            use('levels', {levels: desktopLevels()}),
+            use('bemdecl-from-levels', {
+                levels: bemtreeLevels(),
+                target: '?.bemtree.bemdecl.js'
+            }),
+            use('deps', {
+                bemdeclTarget: '?.bemtree.bemdecl.js',
+                depsTarget: '?.bemtree.deps.js'
+            }),
+            use('files', {
+                depsTarget: '?.bemtree.deps.js',
+                filesTarget: '?.bemtree.files',
+                dirsTarget: '?.bemtree.dirs'
+            }),
+            use('bemtree', {filesTarget: '?.bemtree.files'}),
 
-            /**
-             * Сборка статики + bemhtml из bemtree
-             */
-            [
-                require('./techs/bemjson-from-bemtree'),
-                {
-                    levels: desktopLevels()
-                }
-            ],
-            require('enb/techs/bemdecl-from-bemjson'),
-            require('enb-modules/techs/deps-with-modules'),
-            require('enb/techs/files'),
-            require('enb-bemxjst/techs/bemhtml'),
-            require('enb/techs/css'),
-            require('enb/techs/browser-js'),
-            [
-                require('enb-modules/techs/prepend-modules'),
-                {source: '?.browser.js'}
-            ]
+            /* Сборка статики + bemhtml из bemtree */
+            use('bemjson-from-bemtree', {levels: desktopLevels()}),
+            use('bemdecl-from-bemjson'),
+            use('deps-with-modules'),
+            use('files'),
+            use('bemhtml'),
+            use('css'),
+            use('browser-js'),
+            use('prepend-modules', {source: '?.browser.js'})
         ]);
 
         nodeConfig.addTargets([
@@ -67,11 +36,9 @@ module.exports = function (config) {
             '?.js'
         ]);
     });
-}
+};
 
-/**
- * Список уровней переопределения для разных целей
- */
+/* Список уровней переопределения для разных целей */
 function bemtreeLevels() {
     return [
         app('desktop.blocks')
@@ -90,6 +57,28 @@ function desktopLevels() {
 /**
  * Хелперы
  */
+var techs = {
+    'bemdecl-from-bemjson': require('enb/techs/bemdecl-from-bemjson'),
+    'browser-js':           require('enb/techs/browser-js'),
+    'css':                  require('enb/techs/css'),
+    'deps':                 require('enb/techs/deps'),
+    'files':                require('enb/techs/files'),
+    'levels':               require('enb/techs/levels'),
+    'bemhtml':              require('enb-bemxjst/techs/bemhtml'),
+    'bemtree':              require('enb-bemxjst/techs/bemtree'),
+    'deps-with-modules':    require('enb-modules/techs/deps-with-modules'),
+    'prepend-modules':      require('enb-modules/techs/prepend-modules'),
+    'bemdecl-from-levels':  require('./techs/bemdecl-from-levels'),
+    'bemjson-from-bemtree': require('./techs/bemjson-from-bemtree')
+};
+
+function use(techName, options) {
+    return [
+        techs[techName],
+        options || {}
+    ];
+}
+
 var path = require('path');
 
 /* Локальные блоки */
