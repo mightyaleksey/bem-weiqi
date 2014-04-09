0f21b38f7e331c45711d58f8a7fb3c41dbf6acc8
Alexey Litvinov <sullenor@yandex-team.ru>
2014-04-09 13:15:00 +0400

    Немного доки

--- .enb/use.js
+++ .enb/use.js
@@ -4,14 +4,36 @@ module.exports = use;
 use.extend = extend;
 
 var inherit = require('inherit');
+var _ = require('underscore');
 
+/**
+ * Хранилище технологий
+ * @type {Object}
+ */
 var techs = {};
+/**
+ * Хранилище модулей
+ * @type {Object}
+ */
 var modules = {};
 
+/**
+ * Маленький конструктор для модулей.
+ */
 var Module = inherit({
+    /**
+     * Конструктор.
+     * @param  {String} moduleName Название модуля
+     * @return {Module}
+     */
     __constructor: function (moduleName) {
         this.module = moduleName;
     },
+    /**
+     * Подключает технологию из данного модуля.
+     * @param  {String} techName Название технологии.
+     * @return {Any}
+     */
     req: function (techName) {
         var chunks = [
             this.module,
@@ -23,10 +45,24 @@ var Module = inherit({
     }
 });
 
+/**
+ * Маленький конструктор для технологий.
+ */
 var Tech = inherit({
+    /**
+     * Конструктор.
+     * @param  {String} techName Название технологии.
+     * @return {Tech}
+     */
     __constructor: function (techName) {
         this.tech = techName;
     },
+    /**
+     * Устанавливает или возвращает список целей,
+     * используемых по умолчанию.
+     * @param  {Object}      [targets] Список целей.
+     * @return {Object|Tech}           Если не указан, возвращает сохраненное значение.
+     */
     options: function (targets) {
         if (typeof targets === 'undefined') {
             return this.targets || {};
@@ -35,11 +71,23 @@ var Tech = inherit({
             return this;
         }
     },
+    /**
+     * Подключает технологию.
+     * Использует свойство `module` для получения ссылки на сам модуль.
+     * @return {Any}
+     */
     req: function () {
         return this.module.req(this.tech);
     }
 });
 
+/**
+ * Небольшой сахарок для подключения требуемой технологии для сборки.
+ * @param  {String} techName Название технологии. Например, 'files'.
+ * @param  {String} prefix   Префикс для целей. Вставляется вместо '?'. Например, '__?'.
+ * @param  {Object} options  Список параметров для технологии.
+ * @return {Array}
+ */
 function use(techName, prefix, options) {
     var undef;
 
@@ -60,16 +108,14 @@ function use(techName, prefix, options) {
     if (typeof prefix === 'undefined') {
         targets = options;
     } else {
-        var o = tech.options();
+        var defined = tech.options();
         targets = {};
 
-        Object.keys(o).forEach(function (key) {
-            targets[key] = o[key].replace('?', prefix);
+        _.each(defined, function (val, key) {
+            targets[key] = defined[key].replace('?', prefix);
         });
 
-        Object.keys(options).forEach(function (key) {
-            targets[key] = options[key];
-        });
+        _.extend(targets, options);
     }
 
     return [
@@ -78,6 +124,15 @@ function use(techName, prefix, options) {
     ];
 }
 
+/**
+ * Декларирует список технологий для фунции use.
+ * В качестве аргумента techName можно передавать объект,
+ * в котором в качестве ключей будут выступать названия технологий,
+ * а значения - цели.
+ * @param {String}        moduleName Название модуля. Например, 'enb-bemxjst'.
+ * @param {String|Object} techName   Название технологии. Например, 'files'.
+ * @param {Object}        [options]  Список целей, используемых по умолчанию данной технологией.
+ */
 function extend(moduleName, techName, options) {
     var hash;
 
@@ -90,8 +145,8 @@ function extend(moduleName, techName, options) {
 
     modules[moduleName] || (modules[moduleName] = new Module(moduleName));
 
-    Object.keys(hash).forEach(function (tech) {
-        techs[tech] = new Tech(tech).options(hash[tech]);
+    _.each(hash, function (targets, tech) {
+        techs[tech] = new Tech(tech).options(targets);
         techs[tech].module = modules[moduleName];
     });
 }
